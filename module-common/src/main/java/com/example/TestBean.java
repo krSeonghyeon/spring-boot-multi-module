29d0c17252cf5eb40d5a2f555fd853ae0ce924d8
krSeonghyeon <ya41921174@gmail.com>
2025-01-22 23:46:39 +0900

    feat: 멀티 모듈 추가

---
+++ module-common/src/main/java/com/example/TestBean.java
@@ -0,0 +1,11 @@
+package com.example;
+
+import org.springframework.stereotype.Component;
+
+@Component
+public class TestBean {
+
+    public void dependencyTest() {
+        System.out.println("성공적으로 로딩됐습니다.");
+    }
+}
---
+++ module-rest/src/main/java/com/example/RestApplication.java
@@ -0,0 +1,27 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import jakarta.annotation.PostConstruct;
+
+@SpringBootApplication
+public class RestApplication {
+
+    //의존성 확인을 위한 코드 - 시작
+    private final TestBean testBean;
+
+    public RestApplication(TestBean testBean) {
+        this.testBean = testBean;
+    }
+
+    @PostConstruct
+    public void dependencyTest() {
+        testBean.dependencyTest();
+    }
+    //의존성 확인을 위한 코드 - 끝
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestApplication.class, args);
+    }
+}
